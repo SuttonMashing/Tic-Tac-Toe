7a223d813fec22cfc53c5d3f9646263c89484f16
Jack <jack.sutton@madetech.com>
2022-02-14 13:48:55 +0000

    wrote 2 tests checking position in board, started implementing row finder

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -3,12 +3,24 @@ require "./tictactoe"
 describe "Board" do 
     it "initialises empty 3x3 board" do
         game = Game.new
-        expect(game.board).to eq([["","",""],["","",""],["","",""]]) 
+        expect(game.board("")).to eq([["","",""],["","",""],["","",""]]) 
     end
 
     it "places Cross in centre of board" do
         game = Game.new
-        expect(game.board).to eq([["","",""],["", "X", ""],["","",""]]) 
+        expect(game.board("B2")).to eq([["","",""],["", "X", ""],["","",""]]) 
     end
 
+    it "places Cross in bottom left" do
+        game = Game.new
+        expect(game.board("C1")).to eq([["","",""],["", "", ""],["X","",""]]) 
+    end
+
+end
+
+describe "row finder" do
+    it "gets row given index" do
+        game = Game.new
+        expect(game.row_finder("C")).to eq(2) 
+    end
 end
--- tictactoe.rb
+++ tictactoe.rb
@@ -1,6 +1,32 @@
 class Game
 
-    def board
-        [["","",""],["","",""],["","",""]]
+    def board(position)
+
+        # A, B, C are rows represented by each subarray 
+        # 1, 2, 3 are columns represented by each position in the subarray
+    case position   
+    when "B2"
+        return [["","",""],["","X",""],["","",""]]
+    when "C1"
+        return [["","",""],["", "", ""],["X","",""]]   
+    else 
+        return [["","",""],["","",""],["","",""]]
     end
+
+    end
+    
+    def row_finder(position)
+        row = {:A => 0, :B => 1, :C => 2}
+        position = position.to_sym
+        row[position]
+
+    end    
+
 end
+
+
+
+
+
+
+
